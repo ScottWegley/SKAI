e3c2874035899f0cae6ada4c950541eb43d0e2ab
scottwegley <scottwegley@gmail.com>
2023-08-19 00:04:40 -0700

    empty receive text function

---
+++ src/modules/receiveText.ts
@@ -0,0 +1,7 @@
+import { Client, Message } from "discord.js";
+import { makeError, makeWarning, makeLog, makeHeading, makeSuccess } from "../utils/ColorfulConsole";
+
+/** Parses the message and directs it to the proper channel, creating one if one does not already exists. */
+export default(msg: Message): void => {
+
+}
