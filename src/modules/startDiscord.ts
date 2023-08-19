687b953964450041ca8cd3a45a17ab15a92cd7e8
scottwegley <scottwegley@gmail.com>
2023-08-18 22:14:43 -0700

    added presence and intents to the discord bot

--- src/modules/startDiscord.ts
+++ src/modules/startDiscord.ts
@@ -1,18 +1,25 @@
-import { Client } from "discord.js";
+import { ActivityType, Client } from "discord.js";
 import 'dotenv/config';
 import { makeError, makeWarning, makeLog, makeHeading, makeSuccess } from "../utils/ColorfulConsole";
 import ready from "../events/discord/ready";
 
 
 const DiscordBot = new Client({
-    intents: []
+    presence: {
+        "status": "online",
+        "activities": [
+            { name: "Managing Scott's Life", type: ActivityType.Custom }
+        ]
+    },
+    /* This covers every possible function we could want the Discord bot to have. */
+    intents: ["Guilds", "GuildMembers", "GuildModeration", "GuildEmojisAndStickers", "GuildIntegrations", "GuildWebhooks", "GuildInvites", "GuildVoiceStates", "GuildPresences", "GuildMessages", "GuildMessageTyping", "GuildMessageReactions", "DirectMessages", "DirectMessageReactions", "DirectMessageTyping", "MessageContent", "GuildScheduledEvents", "AutoModerationConfiguration", "AutoModerationExecution"]
 });
 
 /**
  * The startup routine for SKAI.  Currently logs in the Discord bot.
  */
 export default (): void => {
-    
+
     makeLog("Starting Discord Bot");
 
     DiscordBot.login(process.env.DISCORD_TOKEN);
